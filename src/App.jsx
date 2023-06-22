842cb6107cbecd59618a4eb5e98229c1a2490454
ivanlima096 <ivanlima096@gmail.com>
2023-06-22 14:34:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
